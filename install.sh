fa9f70d7e5ae400de218be2c2adee9f620b8f20b
DGR22419 <75600994+dev22419@users.noreply.github.com>
2021-04-06 12:52:18 +0530

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,19 @@
+pkg update -y 
+
+pkg upgrade -y
+
+apt install termux-api -y
+
+pkg install x11-repo -y
+
+pkg install tigervnc -y
+
+pkg install tigervnc -y
+
+pkg install xfce4-session -y
+
+pkg install xfce4-terminal -y
+
+pkg install netsurf -y
+
+cat add.txt >> /data/data/com.termux/files/home/.vnc/xstartup
